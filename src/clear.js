f13f1927f3ec483bd645ae2965f01be4783050f9
Firdavs Allamurotov <93676386+fed1k@users.noreply.github.com>
2022-03-29 20:23:27 +0500

    Delete clear.js

--- src/clear.js
+++
@@ -1,9 +0,0 @@
-import { removeAllList } from './functions.js';
-
-const clearCompleted = () => {
-  const clear = document.getElementById('clear-btn');
-  clear.addEventListener('click', () => {
-    removeAllList();
-  });
-};
-export default clearCompleted;
